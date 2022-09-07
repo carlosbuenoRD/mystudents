3c567529cca5211e167a205aff231e12ac91028e
Carlos Bueno <developer.carlosbueno@gmail.com>
2022-09-07 08:42:34 -0400

    DB config

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connect, disconnect } from '@utils/db'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await connect()
+  await disconnect()
   res.status(200).json({ name: 'John Doe' })
 }
